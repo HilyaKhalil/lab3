69701bebbfd998652345a3761a14c44b84c03faa
Hilya Khalil <hilyaadil@gmail.com>
2021-01-21 13:40:09 -0500

    done

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,24 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Too many questions is the Chan disease.");
 	});
+	$("#testjs").text("Please wait...");
+	$(".jumbotron p").toggleClass("active");
+	$("a.thumbnail").click(projectClick);
+}
 
+function projectClick(e) {
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+		$(description).fadeOut();
+		//description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+	}
+	
+}
+	
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
-}
